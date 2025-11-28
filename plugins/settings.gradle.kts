e6af4c652bbf3274dd79f4309823957054ebac14
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-11-28 14:44:36 +0800

    chore(deps): Bump typesafe-conventions to 0.10.0

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -3,7 +3,7 @@
 rootProject.name = "ProjectKafka_plugins"
 
 plugins {
-    id("dev.panuszewski.typesafe-conventions") version "0.8.0"
+    id("dev.panuszewski.typesafe-conventions") version "0.10.0"
 }
 
 dependencyResolutionManagement {
