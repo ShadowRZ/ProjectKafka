c851628c1fd7b9650a669c1b8e6cc0615dcce82f
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-12-04 23:59:15 +0800

    fix(ci): Add DangerJS file

---
+++ dangerfile.js
@@ -0,0 +1,33 @@
+const { danger, schedule } = require("danger");
+const reporter = require("danger-plugin-lint-report");
+
+// Requires a PR description.
+if (danger.github.pr.body.length < 10) {
+  fail("This pull request needs a description.")
+}
+
+// Checkstyle lints
+schedule(
+  reporter.scan({
+    /**
+     * File mask used to find XML checkstyle reports.
+     */
+    fileMask: "**/reports/**/*.xml",
+
+    /**
+     * If set to true, the severity will be used to switch between the different message formats (message, warn, fail).
+     */
+    reportSeverity: true,
+
+    /**
+     * If set to true, only issues will be reported that are contained in the current changeset (line comparison).
+     * If set to false, all issues that are in modified files will be reported.
+     */
+    requireLineModification: false,
+
+    /**
+     * Optional: If set to true, it will remove duplicate violations.
+     */
+    removeDuplicates: true,
+  })
+);
