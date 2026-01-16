28b9cd8517ffa1a346290b42769c4a7d2a291be4
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-01-16 18:40:29 +0800

    refactor(build): Move plugins to included plugin classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("io.github.shadowrz.projectkafka")
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.kotest) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.metro) apply false
-    alias(libs.plugins.ktlint) apply false
-    alias(libs.plugins.detekt) apply false
 }
 
 tasks.withType<AbstractTestTask>().configureEach {
