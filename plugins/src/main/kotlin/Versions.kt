bc2c87c762822c39ab81e7c64472c9ca3ec5ba9c
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-01-17 22:58:15 +0800

    fix(build): Downgrade Java toolchain to 21

--- plugins/src/main/kotlin/Versions.kt
+++ plugins/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val MIN_SDK = 23
     const val TARGET_SDK = 36
 
-    const val JAVA_VERSION = 25
+    const val JAVA_VERSION = 21
 
     val javaVersion = JavaVersion.toVersion(JAVA_VERSION)
     val jvmTarget = JvmTarget.fromTarget(JAVA_VERSION.toString())
