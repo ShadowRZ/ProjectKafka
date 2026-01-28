269eda70dbe423a9e2109e90ad31fdde69876572
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-01-28 19:17:45 +0800

    Revert "refactor(build): Move plugins to included plugin classpath"
    This reverts commit 28b9cd8517ffa1a346290b42769c4a7d2a291be4.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,17 @@
 plugins {
     alias(libs.plugins.projectkafka)
     alias(libs.plugins.compose.hotreload) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.kotest) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.metro) apply false
+    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.detekt) apply false
 }
 
 tasks.withType<AbstractTestTask>().configureEach {
