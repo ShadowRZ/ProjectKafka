f0de5685e529c5556273a6c9510d1dd24e57748d
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-04-22 20:20:17 +0800

    fix(kafkaui): Clip indicator region to avatar circle

--- libraries/kafkaui/src/commonMain/kotlin/io/github/shadowrz/projectkafka/libraries/kafkaui/AvatarPicker.kt
+++ libraries/kafkaui/src/commonMain/kotlin/io/github/shadowrz/projectkafka/libraries/kafkaui/AvatarPicker.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
@@ -57,7 +59,7 @@ fun AvatarPicker(
             is AvatarPickerState.Selected -> {
                 Avatar(
                     avatar = state.value,
-                    modifier = Modifier.size(size).clickable(onClick = onClick),
+                    modifier = Modifier.size(size).clip(CircleShape).clickable(onClick = onClick),
                 )
                 OutlinedIconButton(
                     modifier = Modifier
