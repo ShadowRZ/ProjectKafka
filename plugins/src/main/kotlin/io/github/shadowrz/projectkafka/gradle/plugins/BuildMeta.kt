12540aa380ce417b658a874e915a6048bcb100ce
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-06-08 09:40:02 +0800

    feat(build): compileSdk: 36 -> 37

--- plugins/src/main/kotlin/io/github/shadowrz/projectkafka/gradle/plugins/BuildMeta.kt
+++ plugins/src/main/kotlin/io/github/shadowrz/projectkafka/gradle/plugins/BuildMeta.kt
@@ -8,7 +8,7 @@ object BuildMeta {
     const val APPLICATION_ID = "io.github.shadowrz.projectkafka"
     const val APPLICATION_NAME = "Project Kafka"
 
-    const val COMPILE_SDK = 36
+    const val COMPILE_SDK = 37
     const val MIN_SDK = 23
     const val TARGET_SDK = 36
 
