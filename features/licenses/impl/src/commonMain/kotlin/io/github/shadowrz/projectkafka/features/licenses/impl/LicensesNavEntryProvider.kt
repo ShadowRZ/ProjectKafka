fbbafb6c45bd47ddb22c6333b2d4cb78e31bb3cb
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-07-11 17:48:13 +0800

    feat(licenses): Add Navigation 3 entry point

---
+++ features/licenses/impl/src/commonMain/kotlin/io/github/shadowrz/projectkafka/features/licenses/impl/LicensesNavEntryProvider.kt
@@ -0,0 +1,27 @@
+package io.github.shadowrz.projectkafka.features.licenses.impl
+
+import androidx.navigation3.runtime.EntryProviderScope
+import androidx.navigation3.runtime.NavKey
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoSet
+import dev.zacsweers.metro.Inject
+import io.github.shadowrz.hanekokoro.framework.runtime.presenter.Presenter
+import io.github.shadowrz.projectkafka.features.licenses.api.LicensesScreen
+import io.github.shadowrz.projectkafka.libraries.architecture.LocalNavigator
+import io.github.shadowrz.projectkafka.libraries.architecture.NavEntryProvider
+
+@Inject
+@ContributesIntoSet(AppScope::class)
+class LicensesNavEntryProvider(private val presenter: Presenter<LicensesState>) : NavEntryProvider {
+    override fun EntryProviderScope<NavKey>.provideEntry() {
+        entry<LicensesScreen> {
+            val state = presenter.present()
+            val navigator = LocalNavigator.current
+
+            LicensesUI(
+                state = state,
+                onBack = { navigator.pop() },
+            )
+        }
+    }
+}
