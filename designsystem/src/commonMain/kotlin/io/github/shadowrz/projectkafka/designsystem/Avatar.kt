fb4d90d1626c7923317185215f6134fc4064051b
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-07-11 09:51:35 +0800

    refactor(designsystem): Use Compose Unstyled UnstyledAvatar component

--- designsystem/src/commonMain/kotlin/io/github/shadowrz/projectkafka/designsystem/Avatar.kt
+++ designsystem/src/commonMain/kotlin/io/github/shadowrz/projectkafka/designsystem/Avatar.kt
@@ -3,17 +3,12 @@ package io.github.shadowrz.projectkafka.designsystem
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import co.touchlab.kermit.Logger
-import coil3.compose.AsyncImagePainter
-import coil3.compose.SubcomposeAsyncImage
-import coil3.compose.SubcomposeAsyncImageContent
+import coil3.compose.rememberAsyncImagePainter
+import com.composeunstyled.UnstyledAvatar
 import io.github.shadowrz.projectkafka.designsystem.internal.EmptyAvatar
 import io.github.shadowrz.projectkafka.designsystem.preview.KafkaPreview
 
@@ -24,48 +19,17 @@ fun Avatar(
     contentDescription: String? = null,
     hideAvatarImage: Boolean = false,
 ) {
-    when {
-        avatar.isNullOrBlank() || hideAvatarImage -> EmptyAvatar(modifier = modifier)
+    val painter = if (hideAvatarImage) null else avatar?.let { rememberAsyncImagePainter(it) }
 
-        else ->
-            ImageAvatar(
-                modifier = modifier,
-                avatar = avatar,
-                contentDescription = contentDescription,
-            )
-    }
-}
-
-@Composable
-private fun ImageAvatar(
-    avatar: String,
-    modifier: Modifier = Modifier,
-    contentDescription: String? = null,
-) {
-    SubcomposeAsyncImage(
-        avatar,
-        contentDescription = contentDescription,
-        contentScale = ContentScale.Crop,
+    UnstyledAvatar(
+        painter = painter,
         modifier = modifier.aspectRatio(1f).clip(CircleShape),
-    ) {
-        val collectedState by painter.state.collectAsState()
-        when (val state = collectedState) {
-            is AsyncImagePainter.State.Success -> {
-                SubcomposeAsyncImageContent()
-            }
-
-            is AsyncImagePainter.State.Error -> {
-                SideEffect {
-                    Logger.e("Error loading avatar ${state.result.request.data}", state.result.throwable)
-                }
-                EmptyAvatar()
-            }
-
-            else -> {
-                EmptyAvatar()
-            }
-        }
-    }
+        contentScale = ContentScale.Crop,
+        contentDescription = contentDescription,
+        underlay = {
+            EmptyAvatar()
+        },
+    )
 }
 
 @Composable
