e2f862e7a47538545da54862dd4c0bc0fd50037e
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-07-19 21:59:04 +0800

    refactor: Use produceState instead of retained flows

--- features/switchsystem/impl/src/commonMain/kotlin/io/github/shadowrz/projectkafka/features/switchsystem/impl/SwitchSystemPresenter.kt
+++ features/switchsystem/impl/src/commonMain/kotlin/io/github/shadowrz/projectkafka/features/switchsystem/impl/SwitchSystemPresenter.kt
@@ -2,20 +2,35 @@ package io.github.shadowrz.projectkafka.features.switchsystem.impl
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.runtime.produceState
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
 import dev.zacsweers.metro.Inject
 import io.github.shadowrz.hanekokoro.framework.runtime.presenter.Presenter
 import io.github.shadowrz.projectkafka.libraries.core.AsyncOutcome
+import io.github.shadowrz.projectkafka.libraries.data.api.System
 import io.github.shadowrz.projectkafka.libraries.data.api.SystemsStore
 import kotlinx.coroutines.flow.map
 
 @Inject
-class SwitchSystemPresenter(systemsStore: SystemsStore) : Presenter<SwitchSystemState> {
-    private val systemsFlow = systemsStore.getSystems().map { AsyncOutcome.Success(it) }
+class SwitchSystemPresenter(private val systemsStore: SystemsStore) : Presenter<SwitchSystemState> {
 
     @Composable
     override fun present(): SwitchSystemState {
-        val systems by systemsFlow.collectAsStateWithLifecycle(initialValue = AsyncOutcome.Loading)
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val lifecycle = lifecycleOwner.lifecycle
+        val systems by
+            produceState<AsyncOutcome<List<System>>>(AsyncOutcome.Loading, lifecycle) {
+                lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    systemsStore
+                        .getSystems()
+                        .map { AsyncOutcome.Success(it) }
+                        .collect {
+                            this@produceState.value = it
+                        }
+                }
+            }
 
         return SwitchSystemState(systems = systems)
     }
--- libraries/kafkastate/impl/src/commonMain/kotlin/io/github/shadowrz/projectkafka/libraries/kafkastate/impl/DefaultMembersPresenter.kt
+++ libraries/kafkastate/impl/src/commonMain/kotlin/io/github/shadowrz/projectkafka/libraries/kafkastate/impl/DefaultMembersPresenter.kt
@@ -1,12 +1,13 @@
 package io.github.shadowrz.projectkafka.libraries.kafkastate.impl
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
 import io.github.shadowrz.projectkafka.libraries.core.AsyncOutcome
 import io.github.shadowrz.projectkafka.libraries.core.coroutine.CoroutineDispatchers
+import io.github.shadowrz.projectkafka.libraries.data.api.Member
 import io.github.shadowrz.projectkafka.libraries.data.api.MembersStore
 import io.github.shadowrz.projectkafka.libraries.di.SystemScope
 import io.github.shadowrz.projectkafka.libraries.kafkastate.api.MembersPresenter
@@ -17,15 +18,21 @@ import kotlinx.coroutines.flow.map
 @Inject
 @ContributesBinding(SystemScope::class)
 class DefaultMembersPresenter(
-    membersStore: MembersStore,
-    coroutineDispatchers: CoroutineDispatchers,
+    private val membersStore: MembersStore,
+    private val coroutineDispatchers: CoroutineDispatchers,
 ) : MembersPresenter {
     @Composable
     override fun present(): MembersState {
-        val members by membersFlow.collectAsState(initial = AsyncOutcome.Loading)
-
+        val members by
+            produceState<AsyncOutcome<List<Member>>>(AsyncOutcome.Loading) {
+                membersStore
+                    .getMembers()
+                    .map { AsyncOutcome.Success(it) }
+                    .flowOn(coroutineDispatchers.computation)
+                    .collect {
+                        this@produceState.value = it
+                    }
+            }
         return MembersState(members = members)
     }
-
-    private val membersFlow = membersStore.getMembers().map { AsyncOutcome.Success(it) }.flowOn(coroutineDispatchers.computation)
 }
