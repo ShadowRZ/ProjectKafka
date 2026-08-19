64b191dacdd7f92a40fcd8b3f95ad7e0451db31e
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-08-19 09:24:28 +0800

    feat(deps): Compose 1.12

--- designsystem/src/commonMain/kotlin/io/github/shadowrz/projectkafka/designsystem/ModalBottomSheet.kt
+++ designsystem/src/commonMain/kotlin/io/github/shadowrz/projectkafka/designsystem/ModalBottomSheet.kt
@@ -13,7 +13,7 @@ import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.contentColorFor
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
@@ -36,7 +36,7 @@ import io.github.shadowrz.projectkafka.designsystem.preview.KafkaPreview
 fun ModalBottomSheet(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
-    sheetState: SheetState = rememberModalBottomSheetState(),
+    sheetState: SheetState = rememberBottomSheetState(initialValue = SheetValue.Hidden),
     shape: Shape = BottomSheetDefaults.ExpandedShape,
     containerColor: Color = BottomSheetDefaults.ContainerColor,
     contentColor: Color = contentColorFor(containerColor),
@@ -78,7 +78,7 @@ fun ModalBottomSheet(
 @OptIn(ExperimentalMaterial3Api::class)
 private fun previewSheetState() =
     SheetState(
-        skipPartiallyExpanded = true,
+        enabledValues = setOf(SheetValue.Expanded),
         initialValue = SheetValue.Expanded,
         positionalThreshold = { 1f },
         velocityThreshold = { 1f },
